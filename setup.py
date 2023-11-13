c86f73e258746e19dd6a1055740f2aae161819b1
Anton <a.baleatolizancos@berkeley.edu>
2023-11-13 14:51:36 -0800

    fixed typo

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/martinjameswhite/direct_sht",
-    packages=['sht']
+    packages=['sht'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
