28111b1e166e94d0c505c84ac62b23aa444729b4
Martin White <martinjameswhite@users.noreply.github.com>
2023-11-13 08:19:22 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="direct_sht",
+    version="0.1",
+    author="Martin White",
+    author_email="mwhite@berkeley.edu",
+    description="Code for direct harmonic transforms of scalar fields",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/martinjameswhite/direct_sht",
+    packages=['sht']
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['numpy','scipy'],
+)
