a25d0e32225aed50a4c598756826a0a26ae27dd9
Martin J White <mwhite@berkeley.edu>
2023-11-17 14:39:31 -0800

    Line wrapping.

--- sht/sht.py
+++ sht/sht.py
@@ -76,98 +76,111 @@ class DirectSHT:
                 Yd[ii,:] *= fact
         self.x,self.Yv,self.Yd = xx,Yv,Yd
         #
-    def __call__(self,theta,phi,wt):
+    def __call__(self,theta,phi,wt,verbose=True):
         """
-        Returns alm for a collection of real-valued points at (theta,phi), in radians, with weights wt.
-        :param theta: 1D numpy array of theta values for each point. Must be between [0,pi].
-        :param phi: 1D numpy array of phi values for each point. Must be between [0,2pi].
+        Returns alm for a collection of real-valued points at (theta,phi),
+        in radians, with weights wt.
+        :param theta: 1D numpy array of theta values for each point.
+         Must be between [0,pi].
+        :param phi: 1D numpy array of phi values for each point.
+         Must be between [0,2pi].
         :param wt: 1D numpy array of weights for each point.
         :return: alm in the Healpix indexing convention,
-                i.e., complex coefficients alm[m * (2*lmax+1-m)/2 + l] with l in [0, lmax] and m in [0, l]
+                 i.e., complex coefficients alm[m*(2*lmax+1-m)/2+l]
+                 with l in [0, lmax] and m in [0, l]
         """
-        assert len(theta)==len(phi) and len(phi)==len(wt), "theta,phi,wt must all be the same length."
-        assert np.all( (theta>=0) & (theta<=np.pi) ), "theta must be in [0,pi)."
-
-        # Get the indexing of ell and m in the Healpix convention for later use
-        ell_ordering, m_ordering = utils.getlm(self.Nell, len(self.Yv[:, 0]))
-        # Eventually, we will need to multiply the alm's by (-1)^{ell-m} for x=cos\theta<0
-        parity_factor = (-1) ** (ell_ordering - m_ordering)
+        assert len(theta)==len(phi) and \
+               len(phi)==len(wt),"theta,phi,wt must be the same length."
+        assert np.all( (theta>0) & (theta<np.pi) ),\
+               "theta must be in (0,pi)."
+        # Get the indexing of ell and m in the Healpix convention for
+        # later use
+        ell_ordering,m_ordering = utils.getlm(self.Nell,len(self.Yv[:, 0]))
+        # Eventually, we will need to multiply the alm's by (-1)^{ell-m}
+        # for x=cos\theta<0
+        parity_factor = (-1)**(ell_ordering - m_ordering)
         # Initialize storage array in dtype compatible with Healpy
         alm_grid_tot = np.zeros(len(ell_ordering), dtype='complex128')
-
-        # We've precomputed P_{\ell m}(x=cos(theta)), so let's work with x thus defined
+        # We've precomputed P_{\ell m}(x=cos(theta)), so let's work
+        # with x thus defined
         x_full = np.cos(theta)
-
-        # Split into positive and negative values of x. We'll treat them separately and rejoin them ar the very end
-        pos_idx, neg_idx = ([i for i, value in enumerate(x_full) if value >= 0],
-                            [i for i, value in enumerate(x_full) if value < 0])
+        # Split into positive and negative values of x.
+        # We'll treat them separately and rejoin them ar the very end
+        pos_idx,neg_idx = ([i for i, value in enumerate(x_full) if value >= 0],\
+                           [i for i, value in enumerate(x_full) if value <  0])
         if pos_idx and neg_idx:
-            which_case = zip([x_full[pos_idx], np.abs(x_full[neg_idx])], [1., parity_factor], [pos_idx, neg_idx])
+            which_case = zip([x_full[pos_idx],np.abs(x_full[neg_idx])],\
+                             [1.,parity_factor],[pos_idx,neg_idx])
         elif pos_idx:
-            which_case = zip([x_full[pos_idx]], [1.], [pos_idx])
+            which_case = zip([x_full[pos_idx]],[1.],[pos_idx])
         elif neg_idx:
-            which_case = zip([np.abs(x_full[neg_idx])], [parity_factor], [neg_idx])
+            which_case = zip([np.abs(x_full[neg_idx])],[parity_factor],\
+                             [neg_idx])
         else:
             raise ValueError("The theta array seems to be empty!")
-
+        #
         if jax_present:
             # We'll want to move big arrays to GPU memory only once
-            Yv_jax = device_put(self.Yv)
+            Yv_jax  = device_put(self.Yv)
             dYv_jax = device_put(self.Yd)
-
         for x, par_fact, idx in which_case:
             t0 = time.time()
             # Sort the data in ascending order of theta
             sorted_idx = np.argsort(x)
             x_data_sorted = x[sorted_idx]; w_i_sorted = wt[idx][sorted_idx]; phi_data_sorted = phi[idx][sorted_idx]
             x_samples = self.x
-
+            #
             t1 = time.time()
-            print("Sorting took ",t1-t0," seconds.",flush=True)
-
+            if verbose: print("Sorting took ",t1-t0," seconds.",flush=True)
+            #
             # Find which spline region each point falls into
             spline_idx = np.digitize(x_data_sorted, x_samples) - 1
             t = x_data_sorted - x_samples[spline_idx]
-
+            #
             t15 = time.time()
-            print("Digitizing took ",t15-t1," seconds.",flush=True)
-
+            if verbose: print("Digitizing took ",t15-t1," seconds.",flush=True)
+            #
             # We now sum up all w_p f(t) in each spline region i
             ms = np.arange(self.Nell, dtype=int)
-            vs_real, vs_imag = [interp.precompute_vs(len(x_samples), spline_idx, phi_data_sorted,
-                                                     w_i_sorted, t, ms, which_part) for which_part in ['cos', 'sin']]
-
+            vs_real, vs_imag = [interp.precompute_vs(len(x_samples),\
+                                spline_idx, phi_data_sorted,\
+                                w_i_sorted,t,ms, which_part) \
+                                for which_part in ['cos', 'sin']]
+            #
             t2 = time.time()
-            print("Precomputing vs took ",t2-t1," seconds.",flush=True)
-
+            if verbose:
+                print("Precomputing vs took ",t2-t1," seconds.",flush=True)
+            #
             if jax_present:
                 # Get a grid of all alm's -- best run on a GPU!
-                get_all_alms_w_jax = vmap(jit(interp.get_alm), in_axes=(0, 0, None, 0))
+                get_all_alms_w_jax = vmap(jit(interp.get_alm),in_axes=(0,0,None, 0))
                 # Notice we put the Ylm and dYlm tables in device memory for a speed boost
                 alm_grid_real = get_all_alms_w_jax(Yv_jax, dYv_jax, vs_real, m_ordering)
                 alm_grid_imag = get_all_alms_w_jax(Yv_jax, dYv_jax, vs_imag, m_ordering)
                 alm_grid = np.array(alm_grid_real, dtype='complex128') - 1j *np.array(alm_grid_imag, dtype='complex128')
-
             else:
                 # JIT compile the get_alm function and vectorize it
                 get_alm_jitted = nb.jit(nopython=True)(interp.get_alm)
-
+                #
                 alm_grid = np.zeros(len(self.Yv[:,0]), dtype='complex128')
                 vs_tot = vs_real - 1j * vs_imag
                 #TODO: parallelize this
-                for i, (Ylm, dYlm, m) in enumerate(zip(self.Yv, self.Yd, m_ordering)):
+                for i, (Ylm, dYlm, m) in \
+                  enumerate(zip(self.Yv,self.Yd,m_ordering)):
                     alm_grid[i] = get_alm_jitted(Ylm, dYlm, vs_tot, m)
-
             # For x<0, we need to multiply by (-1)^{ell-m}
             alm_grid_tot += par_fact * alm_grid
             t3 = time.time()
-            print("Computing alm's took ",t3-t2," seconds.",flush=True)
+            if verbose:
+                print("Computing alm's took ",t3-t2," seconds.",flush=True)
         return(alm_grid_tot)
         #
     def indx(self,ell,m):
         """
-        The index in the grid storing Ylm for ell>=0, 0<=m<=ell. Matches the Healpix convention.
-        Note: this should match the indexing in ext_slow_recurrence and ext_der_slow_recurrence.
+        The index in the grid storing Ylm for ell>=0, 0<=m<=ell.
+        Matches the Healpix convention.
+        Note: this should match the indexing in ext_slow_recurrence
+        and ext_der_slow_recurrence.
         """
         lmax = self.Nell-1
         ii = int(m * (2*lmax+1-m)/2 + ell)
