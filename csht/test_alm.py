d4fe1855ead87c17738645748221504f469535fe
Martin White <martinjameswhite@users.noreply.github.com>
2023-11-22 18:52:34 -0800

    Better formatting.

--- csht/test_alm.py
+++ csht/test_alm.py
@@ -3,10 +3,13 @@
 # Test the SHT class against the SciPy versions.
 # Checks the function values and first derivatives
 # for Ylm(x=cos[theta],0).
+# Also does a sum of Ylm^star for some points as a
+# test.
 #
 #
 import numpy as np
 import time
+
 
 from   sht import DirectSHT
 
@@ -16,9 +19,9 @@ from   sht import DirectSHT
 
 
 if __name__=="__main__":
-    for Nl in [64,128,256]:
-        now   = time.time()
-        sht   = DirectSHT(Nl,2*Nl)
+    for Nl in [128,256]:
+        now = time.time()
+        sht = DirectSHT(Nl,2*Nl)
         print("Nl=",Nl," computation took ",time.time()-now," seconds.",flush=True)
     # Compare Ylm.
     Nl,Nx = 500,1024
@@ -31,7 +34,7 @@ if __name__=="__main__":
     from scipy.special import sph_harm
     #
     Ylb = np.zeros( ((Nl*(Nl+1))//2,Nx) )
-    for ell in range(45):
+    for ell in range(41):
         for m in range(ell+1):
             ii = sht.indx(ell,m)
             for i,x in enumerate(sht.x):
@@ -50,12 +53,30 @@ if __name__=="__main__":
                 print("({:2d},{:2d}):".format(ell,m))
                 print("\t"+lbs)
                 print("\t"+mys)
+    # Now print some values for the derivative (wrt Cos[theta]).
+    print("\nDerivatives:")
+    Ym = np.zeros( ((Nl*(Nl+1))//2,Nx) )
+    Ym[sht.indx( 2, 0),:] = 3*sht.x * np.sqrt(5/4./np.pi)
+    Ym[sht.indx(10,10),:] = -4.19758*sht.x*(1-sht.x**2)**4*np.sqrt(21/4./np.pi)
+    Yd = np.array(sht.Yd[:]).reshape(((Nl*(Nl+1))//2,Nx))
+    for ell,m in zip([2,10],[0,10]):
+        lbs,mys,j = "","",sht.indx(ell,m)
+        for i in ii: lbs+= " {:12.4e}".format(Ym[j,i])
+        for i in ii: mys+= " {:12.4e}".format(Yd[j,i])
+        print("({:2d},{:2d}):".format(ell,m))
+        print("\t"+lbs)
+        print("\t"+mys)
     #
     # Now test the interpolation code.
-    tt = np.linspace(0.6,1.4,100)
-    pp = np.linspace(0.0,3.1,100)
+    print("\nTest sum of Ylms.")
+    tt = np.linspace(0.6,1.4,1000)
+    pp = np.linspace(0.0,3.1,1000)
     wt = np.ones_like(tt)
-    carr,sarr = sht(tt,pp,wt)
-    print("carr=",carr[:10])
-    print("sarr=",sarr[:10])
+    now= time.time()
+    res= sht(tt,pp,wt)
+    print("sht computation took ",time.time()-now," seconds for ",tt.size," points.\n",flush=True)
+    for ell in range(4):
+        for m in range(ell+1):
+            ii = sht.indx(ell,m)
+            print("({:3d},{:3d}) - ".format(ell,m)+str(res[ii]))
     #
