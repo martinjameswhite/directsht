2ba27dd336622955fdf53a7148cccdcd35844a17
Martin White <martinjameswhite@users.noreply.github.com>
2023-12-03 10:40:50 -0800

    Code to test 3js

---
+++ sht/test_3j.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+#
+import numpy as np
+import time
+
+from threej000            import Wigner3j
+from sympy.physics.wigner import wigner_3j
+
+
+
+
+if __name__=="__main__":
+    # Do some tests and timing of the class.
+    for Nl in [1000]:
+        t0 = time.time()
+        temp_3js = Wigner3j(Nl)
+        print("Generating Nl=",Nl," took ",time.time()-t0," seconds.",flush=True)
+        #
+        # Compare to sympy
+        l1=Nl-1
+        l2=Nl-2
+        l3=1
+        our_result   = temp_3js(l1,l2,l3)
+        sympy_result = wigner_3j(l1,l2,l3,0,0,0).n(32)
+        print('Testing l1={}, l2={}, l3={}'.format(l1,l2,l3))
+        print('Fractional difference with sympy: ',
+              (our_result-sympy_result)/(sympy_result+1e-40))
+        print('\n')
+    #
