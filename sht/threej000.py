45a6b1cca7cc8b346817a2d57881ec6956811ded
Anton <a.baleatolizancos@berkeley.edu>
2023-12-03 10:26:00 -0800

    JIT indexing call

--- sht/threej000.py
+++ sht/threej000.py
@@ -42,7 +42,22 @@ def threej000(ell1,ell2,ell3,store):
             store[ii] = res
         return(res)
     #
-
+@numba.jit(nopython=True)
+def get_index_jitted(l1,l2,l3):
+    """
+    Get the index of the Wigner 3j symbol (with m1=m2=m3=0) in the table.
+    :param l1: int. l1
+    :param l2: int. l2
+    :param l3: int. l3
+    :return: int. The index of the Wigner 3j symbol in the table.
+    """
+    # Order ell1, ell2 and ell3 such that j1>=j2>=j3.
+    ells = [l1, l2, l3]
+    j1 = max(ells)
+    j3 = min(ells)
+    j2 = l1 + l2 + l3 - j1 - j3
+    # Work out the index.
+    return(  (j1*(j1+1)*(j1+2))//6 + (j2*(j2+1))//2 + j3 )
 
 class Wigner3j:
     def __init__(self,Nl):
@@ -65,19 +80,14 @@ class Wigner3j:
         #
     def get_index(self,l1,l2,l3):
         """
-        Get the index of the Wigner 3j symbol (with m1=m2=m3=0) in the table.
+        Wrapper function to get the index of the Wigner 3j symbol
+        (with m1=m2=m3=0) in the table.
         :param l1: int. l1
         :param l2: int. l2
         :param l3: int. l3
         :return: int. The index of the Wigner 3j symbol in the table.
         """
-        # Order ell1, ell2 and ell3 such that j1>=j2>=j3.
-        ells = [l1, l2, l3]
-        j1 = max(ells)
-        j3 = min(ells)
-        j2 = l1 + l2 + l3 - j1 - j3
-        # Work out the index.
-        return(  (j1*(j1+1)*(j1+2))//6 + (j2*(j2+1))//2 + j3 )
+        return (get_index_jitted(l1,l2,l3))
         #
     def get_3js(self):
         """A loop to fill the 'store' array.  Can also be used for
