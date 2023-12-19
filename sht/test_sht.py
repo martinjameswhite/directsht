58005f1de1c23fa79e3ae1eb53ea617c3446801a
Anton <a.baleatolizancos@berkeley.edu>
2023-12-18 23:46:27 -0800

    revived tests script

--- sht/test_sht.py
+++ sht/test_sht.py
@@ -36,18 +36,17 @@ if __name__=="__main__":
                 theta     = np.arccos(x)
                 Ylb[ii,i] = np.real( sph_harm(m,ell,0,theta) )
     # Pick some values of x=cos(theta) to compare.
-    ii = np.array([0,Nx//3,2*Nx//3,Nx-1])
-    print("\nCompare our class to SciPy for x=cos(theta)=",sht.x[ii])
+    ix = np.array([0,Nx//3,2*Nx//3,Nx-1])
+    print("\nCompare our class to SciPy for x=cos(theta)=",sht.x[ix])
     # First cross-check some Ylm values for sanity.
     print("\nCheck Ylm values.")
     print("Compare pairs of lines labeled by (ell,m):")
-    Ylm= sht.Yv
     for ell in [1,10,25,40]:
         for m in [0,5,15,25,40]:
             if m<=ell:
                 lbs,mys,j = "","",sht.indx(ell,m)
-                for i in ii: lbs+= " {:12.4e}".format(Ylm[j,i])
-                for i in ii: mys+= " {:12.4e}".format(Ylb[j,i])
+                for i in ix: lbs+= " {:12.4e}".format(sht.get_Ylm(ell,m)[i])
+                for i in ix: mys+= " {:12.4e}".format(Ylb[j,i])
                 print("({:2d},{:2d}):".format(ell,m))
                 print("\t"+lbs)
                 print("\t"+mys)
@@ -55,14 +54,13 @@ if __name__=="__main__":
     # Now print some values for the derivative (wrt Cos[theta]).
     print("\nCheck Ylm derivatives.")
     print("Compare pairs of lines labeled by (ell,m):")
-    Ym = np.zeros_like(sht.Yd)
-    Ym[sht.indx( 2, 0),:] = 3*sht.x * np.sqrt(5/4./np.pi)
-    Ym[sht.indx(10,10),:] = -5.42630291944221461*sht.x*(1-sht.x**2)**4
-    Yd = sht.Yd
-    for ell,m in zip([2,10],[0,10]):
-        lbs,mys,j = "","",sht.indx(ell,m)
-        for i in ii: lbs+= " {:12.4e}".format(Ym[j,i])
-        for i in ii: mys+= " {:12.4e}".format(Yd[j,i])
+    Ym = []
+    Ym.append(3*sht.x * np.sqrt(5/4./np.pi))
+    Ym.append(-5.42630291944221461*sht.x*(1-sht.x**2)**4)
+    for j, (ell,m) in enumerate(zip([2,10],[0,10])):
+        lbs,mys = "",""
+        for i in ix: lbs+= " {:12.4e}".format(Ym[j][i])
+        for i in ix: mys+= " {:12.4e}".format(sht.get_dYlm(ell,m)[i])
         print("({:2d},{:2d}):".format(ell,m))
         print("\t"+lbs)
         print("\t"+mys)
