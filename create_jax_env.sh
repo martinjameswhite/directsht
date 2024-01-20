e2e4bab4ccf8d64106bfe26c168869da40a217a1
Martin White <martinjameswhite@users.noreply.github.com>
2024-01-19 17:13:30 -0800

    Upgrade to cuda12.

--- create_jax_env.sh
+++ create_jax_env.sh
@@ -4,30 +4,27 @@
 #
 # Need to module load these in the scripts that are calling
 # code running under this environment as well.
-module load cudatoolkit/11.7
-module load cudnn/8.9.1_cuda11
+#
+module load cudatoolkit
+module load cudnn/
 module load python
 # Verify the versions of cudatoolkit and cudnn are compatible with JAX
-module list
+# module list
 #
 # Set up the environment, cloning from nersc-mpi4py to get the
 # proper MPI environment.
 conda create --name jax-env --clone nersc-mpi4py
 conda activate jax-env
 conda update --all -y
-conda install numpy scipy ipykernel -y
+conda install -c conda-forge numpy scipy ipykernel -y
 python3 -Xfrozen_modules=off -m ipykernel \
         install --user --name jax-env --display-name JAX-env
 #
 conda install -c conda-forge numba healpy -y
-#conda install -c confa-forge ipython jupyter -y
 #
-#
-# Activate the environment before using pip to install JAX(lib).
-#conda activate jax-env
-# Install the JAX library.
-python3 -m pip install "jaxlib==0.4.7"
-# Install a compatible wheel
-python3 -m pip install --no-cache-dir "jax[cuda11_cudnn82]==0.4.7" \
-  -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
-#
+python3 -m pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+
+
+
+# test installation
+# python -c "from jax.lib import xla_bridge; print(xla_bridge.get_backend().platform); import jax.numpy as jnp; print(jnp.exp(2.34))"
