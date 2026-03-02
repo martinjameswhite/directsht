eec5ed69873a634fcf05762e3ecc4489382cec88
Martin White <martinjameswhite@users.noreply.github.com>
2026-03-02 09:34:38 -0800

    Comparison.

--- csht/threej000.py
+++ csht/threej000.py
@@ -1,52 +1,75 @@
 #!/usr/bin/env python3
 #
-import numpy  as np
-import ctypes as ct
-import os
+import numpy as np
+from sympy.physics.wigner import wigner_3j
 
 
-# Find out where we are.
-fullpath = os.path.dirname(__file__) + "/"
+def threej000(j1,j2,j3):
+    """Returns the absolute value of the Wigner 3j symbol for
+    integer j's and m1=m2=m3=0.
+    Uses https://arxiv.org/pdf/2602.15605 ."""
+    J  = j1+j2+j3
+    if (J%2>0): return(0)
+    # Check the triange condition:
+    tri = (j3<-abs(j1-j2))|(j3>j1+j2)| \
+          (j2<-abs(j1-j3))|(j2>j1+j3)| \
+          (j1<-abs(j2-j3))|(j1>j2+j3)
+    if (tri): return(0)
+    pp = J//2
+    p1 = (-j1+j2+j3)//2
+    p2 = ( j1-j2+j3)//2
+    p3 = ( j1+j2-j3)//2
+    # This part should really be pre-computed and stored, say using
+    # a class with the 3j's returned via a __call__ method.
+    lng = np.zeros(pp+1)
+    for p in range(1,pp+1):
+        lng[p] = lng[p-1]+np.log( 1. - 0.5/p )
+    gg  = np.exp(lng)
+    gfac= gg[p1]*gg[p2]*gg[p3]/gg[pp]
+    tj  = np.sqrt( gfac/(J+1.) ) # Sign ignored.
+    return(tj)
+    #
 
 
+def threej000_recurse(j1,j2,j3):
+    """Returns the Wigner 3j symbol for integer j's and m1=m2=m3=0."""
+    J = j1+j2+j3
+    if (J%2>0):
+        return(0)
+    if (j1>=j2)&(j1>=j3)&(j2>=j3):
+        if (j1==j2)&(j3==0):
+            return( (-1)**j1/np.sqrt(2*j1+1.0) )
+        elif (j1!=j2)&(j3==0):
+            return( 0.0 )
+        else:
+            num = (J-2*j2-1)*(J-2*j3+2)
+            den = (J-2*j2  )*(J-2*j3+1)
+            fac = np.sqrt(float(num)/float(den))
+            return(fac*threej000(j1,j2+1,j3-1))
+    elif (j1>=j2)&(j1>=j3)&(j2< j3):
+        return(threej000(j1,j3,j2))	# No minus sign since J even.
+    elif (j1>=j2)&(j1< j3)&(j2< j3):
+        return(threej000(j3,j1,j2))
+    elif (j1< j2)&(j1>=j3)&(j2>=j3):
+        return(threej000(j2,j1,j3))
+    elif (j1< j2)&(j1< j3)&(j2>=j3):
+        return(threej000(j2,j3,j1))
+    elif (j1< j2)&(j1< j3)&(j2< j3):
+        return(threej000(j3,j2,j1))
+    else:
+        raise RuntimeError
+    #
 
 
-class Wigner3j:
-    def __init__(self,Nl):
-        """
-        Class to compute and save/load Wigner 3j symbols.
-        :param Nl: int. (One plus) the maximum l for which to compute the 3j's
-        """
-        self.Nl    = Nl
-        self.Nsize = self.get_index(Nl,Nl,Nl)
-        # Set up c_double_Array objects for storing three-j symbols.
-        self.store = (ct.c_double*self.Nsize)()
-        self.mylib =  ct.CDLL(fullpath+"threej000_helper.so")
-        self.mylib.make_table(ct.c_int(Nl),ct.byref(self.store))
-        #
-    def __call__(self,l1,l2,l3):
-        """
-        Compute the Wigner 3j symbol for integer l's and m1=m2=m3=0.
-        :param l1: int. l1
-        :param l2: int. l2
-        :param l3: int. l3
-        :return: float. The Wigner 3j symbol
-        """
-        return(self.store[self.get_index(l1,l2,l3)])
-        #
-    def get_index(self,l1,l2,l3):
-        """
-        Get the index of the Wigner 3j symbol (with m1=m2=m3=0) in the table.
-        :param l1: int. l1
-        :param l2: int. l2
-        :param l3: int. l3
-        :return: int. The index of the Wigner 3j symbol in the table.
-        """
-        # Order ell1, ell2 and ell3 such that j1>=j2>=j3.
-        ells = [l1, l2, l3]
-        j1 = max(ells)
-        j3 = min(ells)
-        j2 = l1 + l2 + l3 - j1 - j3
-        # Work out the index.
-        return(  (j1*(j1+1)*(j1+2))//6 + (j2*(j2+1))//2 + j3 )
-        #
+if __name__=="__main__":
+    for j1 in range(10):
+        for j2 in range(10):
+            for j3 in range(10):
+                w3j = float(wigner_3j(j1,j2,j3,0,0,0))
+                w3j = np.abs(w3j)
+                m3j = threej000(j1,j2,j3)
+                J   = j1+j2+j3
+                if J%2==0:
+                    err = (m3j-w3j)/(np.abs(w3j)+0.1)
+                    print("{:2d} {:2d} {:2d} ".format(j1,j2,j3)+\
+                          "{:10.5} {:10.5f} {:10.5f}".format(w3j,m3j,err))
